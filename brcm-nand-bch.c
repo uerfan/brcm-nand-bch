814e81180626095f8d780a5085ee5495cacc1013
Uerfan <844186956@qq.com>
2018-05-30 16:15:07 +0800

    wd

--- brcm-nand-bch.c
+++ brcm-nand-bch.c
@@ -16,13 +16,13 @@
  *
  */
 
-#define BCH_T 8
-#define BCH_M 16
-#define SECTOR_SZ 4096
-#define OOB_SZ 16
-#define SECTORS_PER_PAGE 4
-#define OOB_ECC_OFS 9
-#define OOB_ECC_LEN 16
+#define BCH_T 4
+#define BCH_M 15
+#define SECTOR_SZ 2048
+#define OOB_SZ 8
+#define SECTORS_PER_PAGE 8
+#define OOB_ECC_OFS 0
+#define OOB_ECC_LEN 8
 
 // Wide right shift by 4 bits. Preserves the very first 4 bits of the output.
 static void shift_half_byte(const uint8_t *src, uint8_t *dest, size_t sz)
@@ -43,8 +43,11 @@ int main(int argc, char *argv[])
 	}
 	unsigned poly = argc < 2 ? 0 : strtoul(argv[1], NULL, 0);
 	struct bch_control *bch = init_bch(BCH_M, BCH_T, poly);
-	if (!bch)
+	
+	if (!bch){
+		printf("init_error!");
 		return -1;
+	}
         printf("init-bch success!\n");
 	FILE* fda = fopen(argv[2],"r");
 	FILE* fdb = fopen(argv[3],"w");
@@ -102,14 +105,25 @@ int main(int argc, char *argv[])
 		printf("read error!\n");
 		return 0;
 	}
+	unsigned int i;
 	for (i = 0; i != SECTORS_PER_PAGE; ++i){
-		const uint8_t *sector_data = page_buffer + SECTOR_SZ * i;
+		uint8_t *sector_data = page_buffer + SECTOR_SZ * i;
 		uint8_t *sector_oob = page_buffer + SECTOR_SZ * SECTORS_PER_PAGE + OOB_SZ * i;
-		sector_data[10] = ~sector_data[10];
-		int errloc[BCH_T];
+		sector_data[10] = (sector_data[10] & 0xF0) | ((~(sector_data[10] & 0x0F)) & 0x0F);
+		unsigned int errloc[BCH_T];
 		memset(errloc,0,BCH_T*sizeof(int));
 		int ret = decode_bch(bch,sector_data,SECTOR_SZ,sector_oob,NULL,NULL,errloc);
-		printf("Decode: %d\n",ret);	
+		printf("Decode ret: %d\n",ret);
+		int j=0;
+		for(j=0;j<BCH_T;j++){
+			printf("%d,",errloc[j]);
+			sector_data[errloc[j]/8] ^= 1 << (errloc[j] % 8);
+		}
+		printf("\n");
+		//sector_data[errloc[n]/8] ^= 1 << (errloc[n] % 8);
+		memset(errloc,0,BCH_T*sizeof(int));
+	        ret = decode_bch(bch,sector_data,SECTOR_SZ,sector_oob,NULL,NULL,errloc);			
+		printf("Decode validation ret: %d\n",ret);
 	}
 
 	fclose(fda);
